1a258ac65a4276ea41bc81f98b62d9ea0178ba5f
Gary Holler <gholler76@gmail.com>
2021-03-10 09:18:49 -0500

    updates to a loop solution

--- functions_intermediate-I.py
+++ functions_intermediate-I.py
@@ -10,14 +10,14 @@ def rand_int(min=0, max=100):
 print(rand_int())
 print(rand_int(max=50))
 print(rand_int(min=50))
-print(rand_int(50, 500))
+print(rand_int(500, 50))
 
 # edge case check
 
 
 def rand_int_edge(min=0, max=100):
     if min > max:
-        return f"Check input. {min} cannot be greater than {max}."
+        return f"Check input. Min of {min} cannot be greater than Max of {max}."
     else:
         num = round(random.random() * (max-min) + min)
         return num
